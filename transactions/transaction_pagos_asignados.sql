927a140dd38037f0c78d62464a376d700827e9f7
Nicolas Alalu <nicolasalalu96@gmail.com>
2024-12-30 17:45:26 -0300

    Update transaction_pagos_asignados.sql
    quitamos linea delete

--- transactions/transaction_pagos_asignados.sql
+++ transactions/transaction_pagos_asignados.sql
@@ -4,7 +4,4 @@ SET saldo_restante = f.total - COALESCE((
     FROM public.pagos_facturas pf,
          UNNEST(pf.id_factura, pf.monto_asignado) AS t(id_factura, monto_asignado)
     WHERE f.id::text = t.id_factura
-), 0);
-
-
-delete from pagos_facturas 
+), 0); 
